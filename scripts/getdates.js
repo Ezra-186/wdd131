afb1f875f00fb18da74d6d541a21c77be0eb4b92
Ezra-186 <ezrad186@Gmail.com>
2025-05-08 22:49:53 -0700

    Adding First Project Week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Fill current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// fill last modified Date
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
